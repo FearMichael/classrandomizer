370873d8548cd76dc03ef33ddbf4fec5e2b5d588
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2020-12-05 15:34:29 -0500

    modest adjustments to balancer

--- randomizer.js
+++ randomizer.js
@@ -1,32 +1,52 @@
-const _ = require("lodash");
 const classList = require("./class.json");
+const _ = require("lodash");
 const fs = require("fs");
 const moment = require("moment");
 const weekOf = moment().add(7, "days").startOf("week").add(1, "days").format("MM-DD-YYYY");
-
 const groupSize = 5;
 
+
+// Helper function file
+
 // Level out groups
-const evenGroups = (arr) => {
-    const lastItem = arr.pop();
+const evenGroups = (nestedArr) => {
+
+    const lastItem = nestedArr.pop();
+
     const membersToAdd = groupSize - lastItem.length;
+
     for (let i = 1; i <= membersToAdd; i++) {
-        lastItem.push(arr[i].pop());
+        lastItem.push(nestedArr[i].pop());
     }
-    arr.push(lastItem);
-    return arr;
+
+    nestedArr.push(lastItem);
+
+    return nestedArr;
 }
+
 
 const generateText = (arr) => {
-    let text = `### Groups for ${weekOf}\n`;
+
+    let data = `### Groups for ${weekOf}\n`;
+
     arr.forEach((elem, i) => {
-        text += `\n\n#### Group ${i + 1}\n${elem.map((name, i) => `${i + 1}. ${name}`).join("\n")}`
+
+        data += `\n\n#### Group ${i + 1}\n${elem.map((name, i) => `${i + 1}. ${name}`).join("\n")}`
+
     });
-    return text;
+    return data;
 }
 
+
+/// Executing file
+
 const nameList = classList.map((elem, i) => `${elem.student.firstName} ${elem.student.lastName}`);
+
+
 const groups = evenGroups(_.chunk(_.shuffle(nameList), groupSize));
 
+
 const text = generateText(groups);
+
+
 fs.writeFileSync(`Groups${weekOf}.md`, text);
